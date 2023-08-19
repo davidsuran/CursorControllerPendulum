0cc5aaf4a44df618b1f534e07ec7eb8efed79ad5
david suran <67651970+davidsuran@users.noreply.github.com>
2023-08-19 19:39:27 +0200

    Add project files.

---
+++ CursorControllerPendulum/DoublePendulum.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursorControllerPendulum
+{
+    internal class DoublePendulum
+    {
+        public readonly double Radius1 = 105;
+        public readonly double Radius2 = 175;
+        public double Angle1 = 0;
+        public double Angle2 = 0;
+        public double AngleVelocity1 = 0;
+        public double AngleVelocity2 = 0;
+        static readonly double G = 9.8 / (60);
+
+        public int BallRadius1 => (int)MassToRadius(_mass1);
+        public int BallRadius2 => (int)MassToRadius(_mass2);
+
+        private readonly double _mass1 = 100;
+        private readonly double _mass2 = 10;
+
+        static double Px2 = -1;
+        static double Py2 = -1;
+        public readonly double Cx, Cy;
+        public double X1 = 0;
+        public double Y1 = 0;
+        public double X2 = 0;
+        public double Y2 = 0;
+
+        private double _frameWidth;
+        private double _frameHeight;
+
+        public DoublePendulum(double width, double height)
+        {
+            //double width = 450;
+            Angle1 = Math.PI / 2;
+            Angle2 = Math.PI / 1;
+            Cx = width / 2;
+            Cy = height / 4;
+
+            _frameWidth = width;
+            _frameHeight = height;
+        }
+
+        public void Update()
+        {
+            // https://www.myphysicslab.com/pendulum/double-pendulum-en.html
+
+            double num1 = -G * (2 * _mass1 + _mass2) * Math.Sin(Angle1);
+            double num2 = -_mass2 * G * Math.Sin(Angle1 - 2 * Angle2);
+            double num3 = -2 * Math.Sin(Angle1 - Angle2) * _mass2;
+            double num4 = AngleVelocity2 * AngleVelocity2 * Radius2 + AngleVelocity1 * AngleVelocity1 * Radius1 * Math.Cos(Angle1 - Angle2);
+            double den = Radius1 * (2 * _mass1 + _mass2 - _mass2 * Math.Cos(2 * Angle1 - 2 * Angle2));
+            double angleAcceleration1 = (num1 + num2 + num3 * num4) / den;
+
+            num1 = 2 * Math.Sin(Angle1 - Angle2);
+            num2 = (AngleVelocity1 * AngleVelocity1 * Radius1 * (_mass1 + _mass2));
+            num3 = G * (_mass1 + _mass2) * Math.Cos(Angle1);
+            num4 = AngleVelocity2 * AngleVelocity2 * Radius2 * _mass2 * Math.Cos(Angle1 - Angle2);
+            den = Radius2 * (2 * _mass1 + _mass2 - _mass2 * Math.Cos(2 * Angle1 - 2 * Angle2));
+            double angleAcceleration2 = (num1 * (num2 + num3 + num4)) / den;
+
+            X1 = Radius1 * Math.Sin(Angle1);
+            Y1 = Radius1 * Math.Cos(Angle1);
+
+            X2 = X1 + Radius2 * Math.Sin(Angle2);
+            Y2 = Y1 + Radius2 * Math.Cos(Angle2);
+
+            AngleVelocity1 += angleAcceleration1;
+            AngleVelocity2 += angleAcceleration2;
+            Angle1 += AngleVelocity1;
+            Angle2 += AngleVelocity2;
+
+            Px2 = X2;
+            Py2 = Y2;
+        }
+
+        private double MassToRadius(double x)
+        {
+
+            if (_frameWidth > _frameHeight)
+            {
+                return Normalize(x) * _frameWidth;
+            }
+
+            return Normalize(x) * _frameHeight;
+        }
+
+        private double Normalize(double x)
+        {
+            double xMax, xMin;
+
+            if (_frameWidth > _frameHeight)
+            {
+                xMax = _frameWidth;
+                xMin = 0;
+            }
+            else
+            {
+                xMax = _frameHeight;
+                xMin = 0;
+            }
+
+            return (x - xMin) / (xMax - xMin);
+        }
+    }
+}
---
+++ CursorControllerPendulum/PendulumImage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursorControllerPendulum
+{
+    internal class PendulumImage
+    {
+        public static int x0, y0;
+        int x1, y1, x2, y2;
+        Bitmap bitmap;
+        private Graphics _graphics;
+        Brush blackBrush = Brushes.Black;
+        Brush redBrush = Brushes.Red;
+        Brush blueBrush = Brushes.Blue;
+        Pen blackPen = new Pen(Color.Black);
+        private double _armLengthMultiplier = 2.4;
+
+        private readonly int _ballRadius0;
+        private readonly int _ballRadius1;
+        private readonly int _ballRadius2;
+
+        public PendulumImage(int width, int height, int ballRadius1, int ballRadius2)
+        {
+            bitmap = new Bitmap(width, height);
+            _graphics = Graphics.FromImage(bitmap);
+            x0 = width / 2;
+            y0 = height / 3;
+
+            _ballRadius0 = 5;
+            _ballRadius1 = ballRadius1;
+            _ballRadius2 = ballRadius2;
+        }
+
+        public Bitmap GetImage(DoublePendulum pendulum)
+        {
+
+            x1 = (int)(pendulum.Cx + (int)(pendulum.Radius1 * _armLengthMultiplier * Math.Sin(pendulum.Angle1)));
+            y1 = (int)(pendulum.Cy + (int)(pendulum.Radius1 * _armLengthMultiplier * Math.Cos(pendulum.Angle1)));
+            x2 = x1 + (int)(pendulum.Radius2 * _armLengthMultiplier * Math.Sin(pendulum.Angle2));
+            y2 = y1 + (int)(pendulum.Radius2 * _armLengthMultiplier * Math.Cos(pendulum.Angle2));
+
+            x1 = Math.Clamp(x1, _ballRadius1, bitmap.Width - _ballRadius1);
+            y1 = Math.Clamp(y1, _ballRadius1, bitmap.Height - _ballRadius1);
+            x2 = Math.Clamp(x2, _ballRadius2, bitmap.Width - _ballRadius2);
+            y2 = Math.Clamp(y2, _ballRadius2, bitmap.Height - _ballRadius2);
+
+            _graphics.Clear(Color.White);
+            _graphics.DrawLine(blackPen, (int)pendulum.Cx, (int)pendulum.Cy, x1, y1);
+            _graphics.DrawLine(blackPen, x1, y1, x2, y2);
+
+            _graphics.FillEllipse(blackBrush, (int)pendulum.Cx - 5, (int)pendulum.Cy - _ballRadius0, _ballRadius0 * 2, _ballRadius0 * 2);
+            _graphics.FillEllipse(redBrush, x1 - _ballRadius1, y1 - _ballRadius1, _ballRadius1 * 2, _ballRadius1 * 2);
+            _graphics.FillEllipse(blueBrush, x2 - _ballRadius2, y2 - _ballRadius2, _ballRadius2 * 2, _ballRadius2 * 2);
+            
+            return bitmap;
+        }
+    }
+}
